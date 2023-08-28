9e2f72f3632e30e3989e01c63355fff170d473f1
Manyfaces860 <127599665+Manyfaces860@users.noreply.github.com>
2023-08-28 15:09:04 +0530

    main : working app for sending email side : none

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,20 @@
+import json
 import os
 import smtplib
 from email.message import EmailMessage
 from typing import Union
 import ssl
+from pydantic import BaseModel
 # from dotenv import load_dotenv , find_dotenv
 
-from fastapi import FastAPI
+class Data(BaseModel):
+    name: str
+    organisation: str
+    email: str
+    phone: int = None
+    comment: str = None
+
+from fastapi import FastAPI , Request
 
 app = FastAPI()
 # dotenv_path = find_dotenv()
@@ -19,20 +28,23 @@ EMAIL_RECIEVER = 'agblion9@gmail.com'
 # EMAIL_PASSWORD = os.getenv("EMAIL_PASS")
 # EMAIL_RECIEVER = os.getenv("EMAIL_RECIEVER")
 
-@app.get("/")
-def read_root():
-    return {"Hello": EMAIL_ADDRESS}
+@app.post("/")
+def read_root(data : Data):
+    return {"Hello": data}
 
 
-@app.get("/send")
-def read_item():
-
+@app.post("/send")
+async def read_item(request : Request):
+    anydata = await request.json()  # converts the request body to dict object
+    # print(anydata)
     msg = EmailMessage()
-    msg['Subject'] = 'trial email no.3'
+    msg['Subject'] = 'trial email no.5'
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = EMAIL_RECIEVER
 
-    msg.set_content('This is a first email')
+    body = f"name : {anydata['name']} , organisation : {anydata['organisation']}, email : {anydata['email']}, phone : {anydata['phone']}, comment : {anydata['comment']}"
+
+    msg.set_content(body)
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465 , context=context) as smtp:
